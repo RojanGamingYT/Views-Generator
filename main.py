8371a6abb7d3b30388eae229040d8b235197d05f
RojanGamingYT <71162720+RojanGamingYT@users.noreply.github.com>
2022-11-29 20:03:04 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import requests, time, threading
+from fake_useragent import UserAgent
 
 num_of_views = int(input('Enter the amount of views you would like to generate: '))
 link = input('Enter Link: ')
 
 start_time = time.time()
+ua = UserAgent()
+agent = ua.random
+
+headers = {
+    "User-Agent":
+    agent
+}
 
 def view_item():
-    s = requests.Session()
-    visiting_url = s.get(link)
+        s = requests.Session()
+        visiting_url = s.get(link, headers=headers)
 
 for i in range(num_of_views):
     t = threading.Thread(target=view_item)
